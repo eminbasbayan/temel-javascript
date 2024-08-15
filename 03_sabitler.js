07332931f8ef40e81bec07979a0e96177d3a7487
Emin Basbayan <emin.basbayan@icloud.com>
2024-08-15 16:59:33 +0300

    constants (sabitler)

---
+++ 03_sabitler.js
@@ -0,0 +1,12 @@
+const MAX_DEGER = 31321321;
+
+const AYARLAR = {
+    tema: "karanlık",
+    dil: "tr"
+};
+
+// AYARLAR = "BilGen Yazılım Akademi";
+
+AYARLAR.tema = "açık";
+
+console.log(AYARLAR);
