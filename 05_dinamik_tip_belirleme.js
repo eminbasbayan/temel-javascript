fb6c2e84286403db2004f43e34204b37b1422c86
Emin Basbayan <emin.basbayan@icloud.com>
2024-08-15 17:02:10 +0300

    dinamik tip belirleme

---
+++ 05_dinamik_tip_belirleme.js
@@ -0,0 +1,8 @@
+let degisken = 80; // Number veri tipi
+console.log(typeof degisken);
+
+degisken = "Merhaba"; // String veri tipi
+console.log(typeof degisken);
+
+degisken = true; // Boolean veri tipi
+console.log(typeof degisken);
