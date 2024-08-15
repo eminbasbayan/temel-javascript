316e3df364daed0f8f0624c8f2ce047ba16c4240
Emin Basbayan <emin.basbayan@icloud.com>
2024-08-15 17:00:57 +0300

    primitive (ilkel) veri tipleri

---
+++ 04_primitive_tipler.js
@@ -0,0 +1,24 @@
+let tamSayi = 42;
+let ondalikliSayi = 3.14;
+
+let tekTirnak = "Merhaba";
+let ciftTirnak = "Dünya";
+let tersTirnak = `Merhaba, ${ciftTirnak}!`;
+
+let isTrue = true;
+let isFalse = false;
+
+let tanimsiz;
+console.log(tanimsiz); // undefined
+
+let bosDeger = null;
+console.log(bosDeger);
+
+let sembol1 = Symbol('sembol');
+let sembol2 = Symbol('sembol');
+console.log(sembol1 === sembol2); // false
+
+let buyukSayi = BigInt(123456789012345678901234567890);
+
+console.log(buyukSayi);
+
