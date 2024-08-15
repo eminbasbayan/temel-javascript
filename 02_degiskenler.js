a906a71f9846a8e0b3e48419612b803f95fcf4c8
Emin Basbayan <emin.basbayan@icloud.com>
2024-08-15 16:58:33 +0300

    variables (değişkenler)

---
+++ 02_degiskenler.js
@@ -0,0 +1,18 @@
+var x = 10;
+let y = 10;
+y = 20;
+const z = 10;
+z = 30;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+let isim = "Ali";
+let yas = 25;
+
+
+let tamSayi = 10;           // number (tam sayı)
+let ondalikliSayi = 3.14;   // number (ondalıklı sayı)
+let metin = "Merhaba";      // string (metin)
+let dogruYanlis = true;     // boolean (doğru/yanlış)
