3108cc074ef57ee1da9cadebecf7a2ebc55899cd
Emin Basbayan <emin.basbayan@icloud.com>
2024-08-15 17:05:49 +0300

    arrays (diziler)

---
+++ 08_diziler.js
@@ -0,0 +1,18 @@
+let sayilar = [1, 2, 3, 4, 5];
+let karisik = [1, "iki", { isim: "Emin" }, [3, 4, 5], 6];
+// console.log(karisik[2].isim);
+
+// karisik.push(7);
+
+// console.log(karisik);
+
+let meyveler = ["Elma", "Muz", "Kiraz"];
+
+// for döngüsü
+for (let i = 0; i < meyveler.length; i++) {
+  console.log(meyveler[i]);
+}
+
+// console.log(meyveler[0])
+// console.log(meyveler[1])
+// console.log(meyveler[2])
